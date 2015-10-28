1e120e4851fe6a233c08a5a3006a316e90dcfc64
Krzysiek <krzysiekes.polgmail.com>
2015-10-28 20:20:07 +0100

    stworzenie Obsluga metody, zmiany w obsluga

--- src/Obsluga/Obsluga.java
+++ src/Obsluga/Obsluga.java
@@ -1,11 +1,49 @@
 package Obsluga;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
 public class Obsluga {
 	
 
+	int wybor;		//Zmienna do Scannera
+	Scanner wprowadz = new Scanner(System.in);
+	int koniec = 1;// zmienna do zamykania programu
+	
+	
 	public static void main(String[] args) {
-		System.out.println("Hello Word");
 		
+		Obsluga obsluga = new Obsluga(); // Klasa do pobrania zmiennych
+		ObslugaMetody obslugaMetody = new ObslugaMetody(); 
+	
+		//nieskonczona petla menu
+		while (obsluga.koniec != 0) {
+		
+			obslugaMetody.Menu();
+			
+			//Wybranie wartoœci w menu
+			try{		
+				obsluga.wybor = obsluga.wprowadz.nextInt();
+			}catch(InputMismatchException ex) {
+				System.err.println("Podana wartoœæ nie jest liczb¹ ");
+			}
+			//Menu 
+			switch(obsluga.wybor){
+				case 1: {
+					System.out.println("menu 1");
+					break;
+				}
+				case 2: {
+				
+				
+				}
+				case 3: {
+				
+				
+				}
+				case 4: {
+				
+				}
+			}
+		}
 	}
-
 }
---
+++ src/Obsluga/ObslugaMetody.java
@@ -0,0 +1,19 @@
+package Obsluga;
+
+public class ObslugaMetody {
+	Obsluga obsluga = new Obsluga();
+	
+	public void Menu() {// MENU
+		System.out.println("1.Wyswietl wszystkich");
+		System.out.println("2.Dodaj");
+		System.out.println("3.");
+		System.out.println("4.");
+		System.out.println("5.");
+		System.out.println("6.");
+		System.out.println("7.");
+		System.out.println("8.");
+		System.out.println("9.");
+		System.out.println("0.Wyjscie");
+		System.out.print("Wybierz : . . .");
+	}
+}
