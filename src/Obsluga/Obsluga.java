d2314401625df50b98e43e904a524d60215fa805
Krzysiek <krzysiekes.polgmail.com>
2015-10-30 20:12:22 +0100

    dodawanie metod do obslugi Klubow

--- src/Obsluga/Obsluga.java
+++ src/Obsluga/Obsluga.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import LigaPilkarska.Klub;
 import LigaPilkarska.Pilkarz;
 public class Obsluga {
 	
@@ -12,8 +13,11 @@ public class Obsluga {
 	Scanner wprowadz = new Scanner(System.in);
 	int koniec = 1;// zmienna do zamykania programu
 	int liczbaPilkarzy =0;
+	int liczbaKlubow =0;
 	int idPilk =1;
+	int idKlub =1;
 	ArrayList<Pilkarz> Pilkarze = new ArrayList<Pilkarz>();
+	ArrayList<Klub> Kluby = new ArrayList<Klub>();
 	public static void main(String[] args) {
 		
 		Obsluga obsluga = new Obsluga(); // Klasa do pobrania zmiennych
@@ -38,13 +42,23 @@ public class Obsluga {
 				}
 				case 2: {
 					obslugaMetody.DodaniePilkarza();
+					break;
 				}
 				case 3: {
-				
-				
+					obslugaMetody.UsunPilkarza();
+					break;
 				}
 				case 4: {
-				
+					obslugaMetody.WyswietlKluby();
+					break;
+				}
+				case 5: {
+					obslugaMetody.DodanieKlubu();
+					break;
+				}
+				case 6: {
+					obslugaMetody.UsunKlub();
+					break;
 				}
 			}
 		}
--- src/Obsluga/ObslugaMetody.java
+++ src/Obsluga/ObslugaMetody.java
@@ -3,6 +3,7 @@ package Obsluga;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import LigaPilkarska.Klub;
 import LigaPilkarska.Pilkarz;
 
 public class ObslugaMetody {
@@ -12,10 +13,10 @@ public class ObslugaMetody {
 	public void Menu() {// MENU
 		System.out.println("1.Wyswietl wszystkich pilkarzy");
 		System.out.println("2.Dodaj pilkarza");
-		System.out.println("3.");
-		System.out.println("4.");
-		System.out.println("5.");
-		System.out.println("6.");
+		System.out.println("3.Usuñ Pilkarza");
+		System.out.println("4.Wyœwietl wszystkie Kluby");
+		System.out.println("5.Dodaj klub");
+		System.out.println("6.Usuñ Klub");
 		System.out.println("7.");
 		System.out.println("8.");
 		System.out.println("9.");
@@ -44,21 +45,57 @@ public class ObslugaMetody {
 				nazwisko = wpr.next();
 			System.out.println("Podaj pozycje");
 				pozycja = wpr.next();
-			// Wprowadzanie pilkarza do Bazy Pracownikow
+			// Wprowadzanie pilkarza
 			obsluga.Pilkarze.add(new Pilkarz(obsluga.idPilk, idKlubu, imie, nazwisko, pozycja));
-			obsluga.liczbaPilkarzy++; // Zwiekszenie liczby pracownikow
-			obsluga.idPilk++; // Zwiekszenie id pracownika	
+			obsluga.liczbaPilkarzy++;
+			obsluga.idPilk++;
 		}catch(InputMismatchException ex) {
             System.err.println("Zle podana wartosc");
         }
 	}
-	public void Usun() {
+	public void UsunPilkarza() {
 		int sprawdzanie; 
 		System.out.println("Podaj id Pilkarza,ktorego chcesz usunac : ");
 		sprawdzanie = wpr.nextInt();
 		// jesli podany jest poprawny nr id pilkarza,to zostaje on usuniety
 		obsluga.Pilkarze.remove(sprawdzanie - 1);
 		obsluga.liczbaPilkarzy--;
+	}
+	
+	public void WyswietlKluby() {
+		System.out.println("Kluby : ");
+		for (int i = 0; i < obsluga.Kluby.size(); i++) {
+			obsluga.Kluby.get(i).KlubInfo();
+		}
+	}
+	
+	public void DodanieKlubu() {
+		try{
+			String nazwa;
+			int rokZalozenia;
+			String barwy;	
+			System.out.println("Podaj nazwe Klubu: ");
+				nazwa = wpr.next();
+			System.out.println("Podaj rok zalozenia: ");
+				rokZalozenia = wpr.nextInt();
+			System.out.println("Podaj barwy: ");
+				barwy = wpr.next();
+
+			// Wprowadzanie klubu
+			obsluga.Kluby.add(new Klub(obsluga.idKlub, nazwa, rokZalozenia, barwy));
+			obsluga.liczbaKlubow++; 
+			obsluga.idPilk++; 
+		}catch(InputMismatchException ex) {
+            System.err.println("Zle podana wartosc");
+        }
+	}
+	public void UsunKlub() {
+		int sprawdzanie; 
+		System.out.println("Podaj id Klubu,ktory chcesz usunac : ");
+		sprawdzanie = wpr.nextInt();
+		// jesli podany jest poprawny nr id pilkarza,to zostaje on usuniety
+		obsluga.Kluby.remove(sprawdzanie - 1);
+		obsluga.liczbaKlubow--;
 	}
 	
 	
