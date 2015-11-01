08abfebd893625f471276e37238735a1ad16d2b9
Krzysiek <krzysiekes.polgmail.com>
2015-11-01 17:44:11 +0100

    Dodanie pilkarz.java

---
+++ src/main/com/example/projekt_jud_liga/domain/Pilkarz.java
@@ -0,0 +1,59 @@
+
+public class Pilkarz {
+	 private static int PilkarzID = 0;
+	
+	private int id;
+	private int idKlubu;
+	private String imie;
+	private String nazwisko;
+	private String pozycja;
+	
+	//Setters & Getters
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getImie() {
+		return imie;
+	}
+	public void setImie(String imie) {
+		this.imie = imie;
+	}
+	public String getNazwisko() {
+		return nazwisko;
+	}
+	public void setNazwisko(String nazwisko) {
+		this.nazwisko = nazwisko;
+	}
+	public String getPozycja() {
+		return pozycja;
+	}
+	public void setPozycja(String pozycja) {
+		this.pozycja = pozycja;
+	}
+
+	//Konstruktor
+	public Pilkarz (int id,int idKlubu, String imie, String nazwisko, String pozycja){
+		this.id= ++PilkarzID;
+		this.setIdKlubu(idKlubu);
+		this.setImie(imie);
+		this.setNazwisko(nazwisko);
+		this.setPozycja(pozycja);
+	}
+	//info o pilkarzu
+	public void PilkarzInfo() {
+		String Info = "id: "+id + "id klubu: " + idKlubu + " Imie : " + imie + "   nazwisko : " + nazwisko + "    pozycja : "
+				+ pozycja ;
+		System.out.println(Info);
+	}
+	public int getIdKlubu() {
+		return idKlubu;
+	}
+	public void setIdKlubu(int idKlubu) {
+		this.idKlubu = idKlubu;
+	}
+	
+	
+}
