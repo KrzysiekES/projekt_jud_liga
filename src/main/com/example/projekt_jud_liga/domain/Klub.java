0bfc966c95c5bb21ece553a442f9ed5cc4636300
Krzysiek <krzysiekes.polgmail.com>
2015-11-01 18:26:15 +0100

    dodanie Klub.java

---
+++ src/main/com/example/projekt_jud_liga/domain/Klub.java
@@ -0,0 +1,50 @@
+
+public class Klub {
+	private static int KlubID = 0;
+	
+	private int id;
+	private String nazwa;
+	private int rokZalozenia;
+	private String barwy;
+	
+	//Kontruktor
+	public Klub( int id, String nazwa, int rokZalozenia, String barwy ){
+		this.id=++KlubID;
+		this.setNazwa(nazwa);
+		this.getRokZalozenia();
+		this.setBarwy(barwy);
+	}
+	
+	//Setters & Getters
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNazwa() {
+		return nazwa;
+	}
+	public void setNazwa(String nazwa) {
+		this.nazwa = nazwa;
+	}
+	public int getRokZalozenia() {
+		return rokZalozenia;
+	}
+	public void setRokZalozenia(int rokZalozenia) {
+		this.rokZalozenia = rokZalozenia;
+	}
+	public String getBarwy() {
+		return barwy;
+	}
+	public void setBarwy(String barwy) {
+		this.barwy = barwy;
+	}
+	
+	//info o Klubie
+	public void KlubInfo() {
+		String Info = "id: "+ id + " nazwa: " + "rok za³o¿enia: " + " barwy: " + barwy;
+		System.out.println(Info);
+	}
+
+}
